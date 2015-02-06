bd9bfdd46528ffbc6dc78e641504588b31685aab
Erik Strom <strom2357@gmail.com>
2015-02-06 14:14:05 -0800

    New form and controller updated, have most fields now

--- app/controllers/brews_controller.rb
+++ app/controllers/brews_controller.rb
@@ -21,15 +21,42 @@ class BrewsController < ApplicationController
 		if @brew.save
 			@brew_sesh = BrewSesh.new({user_id: current_user.id, brew_id: @brew.id})
 
-			#dealing with hops:
+			# HOP LOGIC
 			@hop_addition = HopAddition.create!({brew_id: @brew.id})
-			@hops = Hop.create!({name: params[:hop][:name], hop_addition_id: @hop_addition.id})
+			@hop_addition.notes = params[:hop_addition][:notes]
+			@hop_addition.save!
 
+			@hops = Hop.create!({
+				hop_addition_id: @hop_addition.id,
+				name: params[:hop][:name], 
+				form: params[:hop][:form],
+				boil_time: params[:hop][:boil_time],
+				quantity: params[:hop][:quantity]
+				})
+
+			# MALT LOGIC
 			@malt_addition = MaltAddition.create!({brew_id: @brew.id})
-			@malts = Malt.create!({name: params[:malt][:name], malt_addition_id: @malt_addition.id})
+			@malt_addition.notes = params[:malt_addition][:notes]
+			@malt_addition.save!
 
+			@malts = Malt.create!({
+				malt_addition_id: @malt_addition.id,
+				name: params[:malt][:name],
+				quantity: params[:malt][:quantity]
+				})
+
+			# GRAIN LOGIC
 			@grain_mash = GrainMash.create!({brew_id: @brew.id})
-			@grains = Grain.create!({name: params[:grain][:name], mash_id: @grain_mash.id})
+			@grain_mash.notes = params[:grain_mash][:notes]
+			@grain_mash.save!
+
+			@grains = Grain.create!({
+				grain_mash_id: @grain_mash.id,
+				name: params[:grain][:name], 
+				quantity: params[:grain][:quantity],
+				mash_temp: params[:grain][:mash_temp],
+				mash_time: params[:grain][:mash_time]
+				})
 
 			@yeast_pitch = YeastPitch.create!({brew_id: @brew.id})
 			@yeasts = Yeast.create!({name: params[:yeast][:name], yeast_pitch_id: @yeast_pitch.id})
